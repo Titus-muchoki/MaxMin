0a882424de6b768386fb87ca5348eacdbfadfa3c
kajela <titoyut56@gmail.com>
2022-06-07 01:53:20 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Math.max(4,8));
+        System.out.println(Math.sqrt(324));
+        System.out.println(Math.min(4,18));
+        System.out.println(Math.abs(-4.8));
     }
 }
